a881e1da7f485f3b43361620b88f739afa2f4c3e
pierobernazar <bernazar.piero@gmail.com>
2025-04-13 15:54:49 -0400

    Update App.jsx

--- simulador-proyectos/src/App.jsx
+++ simulador-proyectos/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import questionsBank from "./questionsBank_112";
+import questionsBank from "./questionsBank";
 
 function App() {
   const [questions, setQuestions] = useState([]);
